570ed553af08fb5135bc69cd2f63298df36f350c
Jon <jondekerh@gmail.com>
2018-11-30 21:23:41 -0800

    got the responses in one array

--- monitor.js
+++ monitor.js
@@ -7,14 +7,18 @@ const nikeURLs = [
   'https://api.nike.com/product_feed/threads/v2/?anchor=150&count=50&filter=marketplace%28US%29&filter=language%28en%29&filter=inStock%28true%29&filter=productInfo.merchPrice.discounted%28false%29&filter=channelId%28010794e5-35fe-4e32-aaff-cd2c74f89d61%29&filter=exclusiveAccess%28true%2Cfalse%29&fields=active&fields=id&fields=lastFetchTime&fields=productInfo&fields=publishedContent.nodes&fields=publishedContent.properties.coverCard&fields=publishedContent.properties.productCard&fields=publishedContent.properties.products&fields=publishedContent.properties.publish.collections&fields=publishedContent.properties.relatedThreads&fields=publishedContent.properties.seo&fields=publishedContent.properties.threadType&fields=publishedContent.properties.custom'
 ];
 
-var refreshDelay = 300000 //default is 5 mins, feel free to change
+var cycle = 0; //dont change this
+var refreshDelay = 300000; //default is 5 mins, feel free to change
 var currentStock = [];
 var newStock = [];
 
 
 function monitor() {
 
+  var completeShoeList = [];
+
   for(i in nikeURLs) {
+    console.log(i);
 
     var options = {
       url: nikeURLs[i],
@@ -25,23 +29,31 @@ function monitor() {
     request(options, function(err, response, body) {
       if(err) { console.log(err); return; }
       console.log("RESPONSE:" + response.statusCode);
-      var parsedBody = JSON.parse(body);
+      let json = JSON.parse(body);
 
       try {
-        for (x = 0; x < parsedBody.objects.length; x++) {
-          if(!parsedBody.objects[x].publishedContent.properties.custom.restricted) {
-          console.log('https://www.nike.com/launch/t/' + parsedBody.objects[x].publishedContent.properties.seo.slug);
-          console.log(x)
+        for (x in json.objects) {
+          if(!json.objects[x].publishedContent.properties.custom.restricted) {
+            completeShoeList.push(json.objects[x]);
           }
         }
       } catch(ex) {
         console.log(ex)
         console.log('this shouldn\'t be an issue, moving on...')
       }
+    })
+  };
 
-    });
-  }
 
+  if (cycle == 0) {
+    cycle++
+    currentStock = completeShoeList;
+  } else {
+    cycle++
+    newStock = completeShoeList;
+  };
 }
 
 monitor();
+setTimeout(() => {console.log(currentStock.length)}, 10000);
+setTimeout(() => {console.log(newStock.length)}, 10000);
