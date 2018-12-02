06b50db0baefd757e74a1aee8af95edcf013ad35
Jon <jondekerh@gmail.com>
2018-12-02 15:11:26 -0800

    async portion of requests works, still need to make sure return only happens AFTER they're done

--- monitor.js
+++ monitor.js
@@ -16,45 +16,82 @@ var newStock = [];
 
 function monitor() {
 
-  var completeShoeList = [];
+  var completeArr = [];
 
-  for(i in nikeURLs) {
-    console.log(i);
-
-    var options = {
-      url: nikeURLs[i],
-      method: 'GET',
-      timeout: 60000
-    }
-
-    request(options, function(err, response, body) {
-      if(err) { console.log(err); return; }
-      console.log("RESPONSE:" + response.statusCode);
-      let json = JSON.parse(body);
-
-      try {
-        for (x in json.objects) {
-          if(!json.objects[x].publishedContent.properties.custom.restricted) {
-            completeShoeList.push(json.objects[x]);
-          }
+  rp.get(nikeURLs[0])
+  .then((body) => {
+    console.log('first!')
+    let json = JSON.parse(body);
+    try {
+      for (x in json.objects) {
+        if(!json.objects[x].publishedContent.properties.custom.restricted) {
+          completeArr.push(json.objects[x]);
         }
-      } catch(ex) {
-        console.log(ex)
-        console.log('this shouldn\'t be an issue, moving on...')
       }
-    })
-  };
+    } catch(ex) {
+      console.log(ex)
+      console.log('this shouldn\'t be an issue, moving on...')
+    }
+    console.log(completeArr.length);
+  })
+  .then(() => {
+    return rp.get(nikeURLs[1]);
+  })
+  .then((body) => {
+    console.log('second!')
+    let json = JSON.parse(body);
+    try {
+      for (x in json.objects) {
+        if(!json.objects[x].publishedContent.properties.custom.restricted) {
+          completeArr.push(json.objects[x]);
+        }
+      }
+    } catch(ex) {
+      console.log(ex)
+      console.log('this shouldn\'t be an issue, moving on...')
+    }
+    console.log(completeArr.length);
+  })
+  .then(() => {
+    return rp.get(nikeURLs[2]);
+  })
+  .then((body) => {
+    console.log('third!')
+    let json = JSON.parse(body);
+    try {
+      for (x in json.objects) {
+        if(!json.objects[x].publishedContent.properties.custom.restricted) {
+          completeArr.push(json.objects[x]);
+        }
+      }
+    } catch(ex) {
+      console.log(ex)
+      console.log('this shouldn\'t be an issue, moving on...')
+    }
+    console.log(completeArr.length);
+  })
+  .then(() => {
+    return rp.get(nikeURLs[3]);
+  })
+  .then((body) => {
+    console.log('fourth!')
+    let json = JSON.parse(body);
+    try {
+      for (x in json.objects) {
+        if(!json.objects[x].publishedContent.properties.custom.restricted) {
+          completeArr.push(json.objects[x]);
+        }
+      }
+    } catch(ex) {
+      console.log(ex)
+      console.log('this shouldn\'t be an issue, moving on...')
+    }
+    console.log(completeArr.length);
+  })
+  .catch ((err) => {
+    console.log(err);
+  })
+  return completeArr;
+};
 
-
-  if (cycle == 0) {
-    cycle++
-    currentStock = completeShoeList;
-  } else {
-    cycle++
-    newStock = completeShoeList;
-  };
-}
-
-monitor();
-setTimeout(() => {console.log(currentStock.length)}, 10000);
-setTimeout(() => {console.log(newStock.length)}, 10000);
+console.log(monitor());
